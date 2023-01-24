ecccce53bd3d2960ece99fa19e5a531d6a1d4046
parvathi <sriparvathi.kalakoti@gmail.com>
2023-01-24 13:21:03 -0600

    link added

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,7 +1,9 @@
 import Todo from "./Todo"
 import {getTodos} from "../services/todos-api";
 import {useState, useEffect} from 'react';
+import { Link } from "react-router-dom";
 import Create from "./CreateTodo"
+
 export default function Todos() {
     const [todos, setTodos] = useState([]) // useState hook allow storing the data from json into a variable called todos using settodos.
     //// useeffect hook will take 2 arguments, 1 is arrow function and the other is dependency variable will excute after rendering the component. if no value in dependency variable array then useeffect executes only once.
@@ -19,9 +21,12 @@ export default function Todos() {
             {todos.map((todo) => {
                 return (
                     <div>
-                        {todo.complete ? <li style = {{textDecorationLine: "line-through", Color: "white"}}><a href={`/${todo._id}`}>{todo.description}</a></li> : 
-                        <li style = {{backgroundColor: "green"}}><a href={`/${todo._id}`}>{todo.description}</a></li>}
+                    <Link to={`/${todo._id}`}>{todo._id}</Link>
                     </div>
+                    // <div>
+                    //     {todo.complete ? <li style = {{textDecorationLine: "line-through", Color: "white"}}><a href={`/${todo._id}`}>{todo.description}</a></li> : 
+                    //     <li style = {{backgroundColor: "green"}}><a href={`/${todo._id}`}>{todo.description}</a></li>}
+                    // </div>
                     
                 )
             })}
